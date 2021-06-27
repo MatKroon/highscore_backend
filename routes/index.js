a1913fb601ac20882efd862eb8c6bf327478d6b7
MatKroon <mer.andersson@gmail.com>
2021-06-27 14:42:26 +0200

    Sequelize fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+// const { Client } = require("pg");
+
+ const Games = require('../models/Games.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  router.get('/', async function (req, res) {
+  
+    const games = await Games.findAll();
+
+    // const client = new Client(req.app.locals.client);
+
+  // client.connect();
+
+    // client.query("SELECT id, name, description, imageUrl FROM games", (err, result) => {
+
+     
+
+      // const games = result.rows;
+    
+      res.render('index', {
+        title: 'High Score',
+        games
+      });
+       client.end();
+    });
+   
+// });
 
 module.exports = router;
+
+
+
+  
