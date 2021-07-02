d5d042539961bd43bae64a3a6b92451ad00da613
MatKroon <mer.andersson@gmail.com>
2021-07-02 02:32:14 +0200

    basic start mongoose

--- models/Games.js
+++ models/Games.js
@@ -1,45 +1,11 @@
-MOONGOSE grg;
+const mongoose = require("mongoose");
 
-class Games extends Model {}
+const Games = mongoose.Schema({
+  name: String,
+  description: String,
+  genre: String,
+  imageurl: String,
+  url_slug: String,
+});
 
-const sequelize = new Sequelize(
-  `postgres://${process.env.PSQL_USER}:${process.env.PSQL_PASS}@${process.env.PSQL_HOST}:${process.env.PSQL_PORT}/${process.env.PSQL_DB}`
-);
-
-Games.init(
-  {
-    id: {
-      type: DataTypes.NUMBER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    genre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    url_slug: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    imageurl: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: "games",
-    timestamps: false,
-  }
-);
-
-module.exports = Games;
+module.exports = mongoose.model("games", Games);
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,52 @@
 var express = require("express");
 var router = express.Router();
 
-// const { Client } = require("pg");
-
-const Games = require("../models/Games.js");
-const HighScores = require("../models/HighScores.js");
-const Players = require("../models/Players.js");
-// const { QueryTypes } = require('sequelize');
-Games.belongsToMany(Players, { through: HighScores });
-Players.belongsToMany(Games, { through: HighScores });
-//  Games.hasMany(HighScores);
-//  HighScores.belongsTo(Games,{foreignKey:'game_id'});
+const mongoose = require("mongoose");
+const Games = require("../models/Games");
+const mongo = "mongodb://127.0.0.1/highscore";
 
 /* GET home page. */
-router.get("/", async function (req, res) {
-  // SELECT games.name AS game, imageurl, points, firstname, surname, date
-  // FROM games
-  // JOIN highscores
-  //   ON games.id = highscores.game_id
-  //  JOIN players
-  //   ON highscores.player_id = players.id
+try {
+  router.get("/", async function (req, res) {
+    mongoose.connect(mongo, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  try {
-    let games = await Games.findAll({ include: Players });
+    Games.find({}, (err, games) => {
+      // let games = await Games.findAll({ include: Players });
 
-    games = JSON.parse(JSON.stringify(games));
+      // games = JSON.parse(JSON.stringify(games));
 
-    games = games.map((game) => {
-      game.Players = game.Players.sort((a, b) => {
-        return b.HighScores.points - a.HighScores.points;
+      // games = games.map((game) => {
+      //   game.Players = game.Players.sort((a, b) => {
+      //     return b.HighScores.points - a.HighScores.points;
+      //   });
+      //   return game;
+      // });
+      // console.log(games);
+
+      // games = games.sort((a, b) => {
+      //   if (typeof b.Players === "undefined") {
+      //     return 0;
+      //   }
+      //   if (typeof a.Players === "undefined") {
+      //     return 1;
+      //   }
+      //   return (
+      //     new Date(b.Players[0].HighScores.date) -
+      //     new Date(a.Players[0].HighScores.date)
+      //   );
+      // });
+
+      res.render("index", {
+        title: "Senaste highscores",
+        games,
       });
-      return game;
     });
-    console.log(games);
-
-    games = games.sort((a, b) => {
-      if (typeof b.Players === "undefined") {
-        return 0;
-      }
-      if (typeof a.Players === "undefined") {
-        return 1;
-      }
-      return (
-        new Date(b.Players[0].HighScores.date) -
-        new Date(a.Players[0].HighScores.date)
-      );
-    });
-
-    res.render("index", {
-      title: "Senaste highscores",
-      games,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+  });
+} catch (err) {
+  console.log(err);
+}
 
 module.exports = router;
