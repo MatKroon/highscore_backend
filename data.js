fb41cb0be2ff01c1516d336ef9d2b28e32c970ba
MatKroon <mer.andersson@gmail.com>
2021-07-07 23:42:17 +0200

    small edit

--- data.js
+++ data.js
@@ -1,95 +1,44 @@
-db.games.insertMany({
-        name: 'Tetris',
-        description: 'Ett spel där block placeras tillsammans för att bilda en full rad som då försvinner',
-        genre: 'Datorspel',
-        image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Emacs_Tetris_vector_based_detail.svg/480px-Emacs_Tetris_vector_based_detail.svg.png',
-        url_slug: 'tetris',
-}, {
-        
+db.games.insertMany(
+  {
+    name: "Tetris",
+    description:
+      "Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.r",
+    genre: "Datorspel",
+    image_url: "https://via.placeholder.com/300?text=Tetris",
+    url_slug: "tetris",
+  },
+  {
+    name: "Schack",
+    description:
+      "Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.r",
+    genre: "Datorspel",
+    image_url: "https://via.placeholder.com/300?text=Schack",
+    url_slug: "schack",
+  }
+);
 
-    });
+db.players.insertMany(
+  {
+    firstname: "Kalle",
+    surname: "Anka",
+    email: "mail@123.se",
+  },
+  {
+    firstname: "Kajsa",
+    surname: "Anka",
+    email: "mail@123.se",
+  }
+);
 
-db.players.insertMany({
-    firstname: 'Kalle',
-    surname: 'Anka',
-    email: 'mail@123.se',
-}, {
-    firstname: 'Kalle',
-    surname: 'Anka',
-    email: 'mail@123.se',
+// db.highscore.highscores.insert({
+//     game_id: ObjectId(''),
+//     player_id: ObjectId(''),
+//     points: 0,
+//     date: ''
+// });
+
+db.users.insert({
+  username: "mattias",
+  password: "123",
+  email: "mer.andersson@gmail.com",
 });
-
-db.highscore.highscores.insert({
-    game_id: ObjectId(''),
-    player_id: ObjectId(''),
-    points: ,
-    date: ''
-});
-
-db.users.insert({username:"mattias", password:"123", email:"mer.andersson@gmail.com"});
-
-// what do i need to do?
-
-// show games with there higest scores
-// show highscores connected to a games
-
-// show users admin
-// show games admin
-// show highscores admin
-
-
-
-
-// -- CREATE TABLE games (
-// --   id SERIAL PRIMARY KEY,
-// --   name VARCHAR(50) NOT NULL,
-// --   description VARCHAR(250) NOT NULL,
-// --   genre VARCHAR(50) NOT NULL,
-// --   imageurl VARCHAR(250) NOT NULL,
-// --   url_slug VARCHAR(50) NOT NULL,
-// --   CONSTRAINT url_unique UNIQUE (url_slug)
-// -- );
-
-// -- -- ALTER TABLE games add column url_slug VARCHAR(50);
-
-// -- CREATE TABLE players (
-// --   id SERIAL PRIMARY KEY,
-// --   firstname VARCHAR(50) NOT NULL,
-// --   surname VARCHAR(50) NOT NULL,
-// --   email VARCHAR(250) NOT NULL
-// -- );
-
-// -- CREATE TABLE highscores2 (
-// --   id SERIAL PRIMARY KEY,
-// --   game_id INTEGER NOT NULL,
-// --   player_id INTEGER NOT NULL,
-// --   points INTEGER NOT NULL,
-// --   date DATE NOT NULL,
-// --   FOREIGN KEY (player_id) REFERENCES players(id),
-// --   FOREIGN KEY (game_id) REFERENCES games(id)
-// --   ON DELETE CASCADE
-// -- );
-
-// -- INSERT INTO games 
-// -- (name, description, genre, imageurl, url_slug) 
-// -- VALUES 
-// -- ('Tetris','Lorum ipsum dolor ','mobilspel', 'https://scienceline.org/wp-content/uploads/2020/01/tetris.jpg','tetris'),
-// -- ('Mariocart','Lorum ipsum dolor ','tv-spel', 'https://images-na.ssl-images-amazon.com/images/I/71eq0cyHRUS._AC_SL1500_.jpg','mariocart');
-
-
-
-
-// -- INSERT INTO players 
-// -- (firstname, surname, email) 
-// -- VALUES 
-// -- ('Kalle','Anka','kalle.anka@gmail.com'), 
-// -- ('Test','Testsson','test.test@gmail.com');
-
-
-// -- INSERT INTO highscores 
-// -- (game_id, player_id, points, date) 
-// -- VALUES 
-// -- (1, 2, 1000, '2019-02-02'), 
-// -- (2, 1, 4000, '2019-02-02'),
-// -- (2, 2, 1500, '2019-02-02'),
-// -- (1, 2, 1500, '2020-02-02');
